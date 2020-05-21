4ff673ce57fd784ca79c6783b6a1748037df277e
Dario-0 <dbaurcicoa.2002@gmail.com>
2020-05-21 10:33:23 +0200

    Error handling

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ require('dotenv').config();
 // routers
 const todoRouter = require('./routers/todo');
 
-const PORT = process.env.PORT || 4000;
-
 const app = express();
 app.use(helmet());
 app.use(morgan('tiny'));
@@ -14,5 +12,27 @@ app.use(express.json());
 
 app.use('/todo', todoRouter);
 
+app.get('/error', (req, r) => {
+  const err = new Error('Congrats, you are getting an error!');
+  err.status = 418;
+  throw err;
+});
+
+app.use((req, res, next) => {
+  const err = new Error(`Route for ${req.method} ${req.originalUrl} not found`);
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500);
+  res.json({
+    error: err.message,
+    status: err.status || 500
+  });
+});
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT);
 console.log('App listening on port', PORT);
--- src/routers/todo.js
+++ src/routers/todo.js
@@ -4,18 +4,22 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({ msg: "Todos!" })
-})
+});
+
 router.get('/:id', (req, res) => {
   res.json({ msg: `Requested todo number ${req.params.id}` })
-})
+});
+
 router.post('/', (req, res) => {
   res.json({ msg: "Requested to make a new todo" })
-})
+});
+
 router.put('/:id', (req, res) => {
   res.json({ msg: `Requested to update todo number ${req.params.id}` })
-})
+});
+
 router.delete('/:id', (req, res) => {
   res.json({ msg: `Requested to delete todo number ${req.params.id}` })
-})
+});
 
 module.exports = router
